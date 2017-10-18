269fb18dfdb03a50aa83af570803b9392ddf99cb
Dariusz Szut <dew326@gmail.com>
2017-10-18 14:21:08 +0200

    Install react modules

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     output: {
         filename: '[name].module.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'Resources/public/js'),
         library: ['eZ', 'modules', '[name]'],
         libraryTarget: 'umd',
         libraryExport: 'default',
