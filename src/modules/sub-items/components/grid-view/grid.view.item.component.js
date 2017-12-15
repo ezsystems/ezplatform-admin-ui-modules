ac2e2d5cf697296010d4054eeafa12a2f6a1a9c7
Piotr Nalepa <piotr.nalepa@ez.no>
2017-12-15 20:17:30 +0100

    fixup

--- src/modules/sub-items/components/grid-view/grid.view.item.component.js
+++ src/modules/sub-items/components/grid-view/grid.view.item.component.js
@@ -4,23 +4,33 @@ import PropTypes from 'prop-types';
 import './css/grid.view.item.component.css';
 
 const GridViewItemComponent = (props) => {
-    const {content, contentTypesMap, labels, locationViewLink, location} = props;
+    const {content, locationViewLink, location} = props;
     const imageField = content.CurrentVersion.Version.Fields.field.find(item => item.fieldTypeIdentifier === 'ezimage');
     const imageClassName = 'c-grid-view-item__image';
-    let image = <div className={`${imageClassName} ${imageClassName}--none`}>{labels.noImage}</div>;
+    const hasImage = imageField && imageField.fieldValue && imageField.fieldValue.uri && imageField.fieldValue.path;
+    let image = (
+        <div className={`${imageClassName} ${imageClassName}--none`}>
+            <svg className="ez-icon">
+                <use xlinkHref="/bundles/ezplatformadminui/img/ez-icons.svg#file"></use>
+            </svg>
+        </div>
+    );
+    let contentTypeIcon = '';
 
-    if (imageField && imageField.fieldValue && imageField.fieldValue.uri && imageField.fieldValue.path) {
+    if (hasImage) {
         image = <img className={imageClassName} src={imageField.fieldValue.uri} alt={`${imageField.fieldValue.path}`} />;
+        contentTypeIcon = (
+            <div className="c-grid-view-item__content-type">
+                <svg className="ez-icon">
+                    <use xlinkHref="/bundles/ezplatformadminui/img/ez-icons.svg#file"></use>
+                </svg>
+            </div>
+        );
     }
 
     return (
         <a className="c-grid-view-item" href={locationViewLink.replace('{{locationId}}', location.id)}>
-            <div className="c-grid-view-item__content-type">
-                <svg className="ez-icon">
-                    <use xlinkHref="/bundles/ezplatformadminui/img/ez-icons.svg#category"></use>
-                </svg>
-                {contentTypesMap[content.ContentType._href].identifier}
-            </div>
+            {contentTypeIcon}
             <div className="c-grid-view-item__image-wrapper">{image}</div>
             <div className="c-grid-view-item__title">{content.Name}</div>
         </a>
