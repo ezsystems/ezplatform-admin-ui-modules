4fdbdd7c8ad26bdc5b1b1a43be76020396c2e5c2
Dariusz Szut <dew326@gmail.com>
2017-12-18 21:28:42 +0100

    Fix switching to grid view

--- src/modules/sub-items/components/grid-view/grid.view.item.component.js
+++ src/modules/sub-items/components/grid-view/grid.view.item.component.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import './css/grid.view.item.component.css';
 
 const GridViewItemComponent = (props) => {
-    const {content, location} = props;
+    const {content, location, handleViewItem} = props;
     const imageField = content.CurrentVersion.Version.Fields.field.find(item => item.fieldTypeIdentifier === 'ezimage');
     const imageClassName = 'c-grid-view-item__image';
     const hasImage = imageField && imageField.fieldValue && imageField.fieldValue.uri && imageField.fieldValue.path;
@@ -29,7 +29,7 @@ const GridViewItemComponent = (props) => {
     }
 
     return (
-        <div className="c-grid-view-item" onClick={this.props.handleViewItem.bind(null, location.id)}>
+        <div className="c-grid-view-item" onClick={handleViewItem.bind(null, location.id)}>
             {contentTypeIcon}
             <div className="c-grid-view-item__image-wrapper">{image}</div>
             <div className="c-grid-view-item__title">{content.Name}</div>
