b82d885a5ca41d2fe271c9a7f718ba675669e3c4
Dawid Parafiński <dawid.parafinski@ez.no>
2018-01-24 08:40:17 +0100

    [EZP-28729] remove 'missplaced' function call.

--- src/modules/multi-file-upload/multi.file.upload.module.js
+++ src/modules/multi-file-upload/multi.file.upload.module.js
@@ -179,7 +179,7 @@ export default class MultiFileUploadModule extends Component {
 
         return (
             <div className="m-mfu__btn--upload" title={this.props.uploadBtnLabel} onClick={this.showUploadPopup.bind(this, [])}>
-                <svg className="ez-icon">extractDroppedFilesList(2)
+                <svg className="ez-icon">
                     <use xlinkHref="/bundles/ezplatformadminui/img/ez-icons.svg#upload"></use>
                 </svg>
             </div>
