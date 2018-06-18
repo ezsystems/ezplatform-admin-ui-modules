59e417cac31db3ee3e3e339697e5e0727df68e27
Jakub Brzegowski <symfiz@gmail.com>
2018-06-18 14:46:31 +0200

    Split Webpack configuration file into 3 files: common, production, developement.

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: {
-        'SubItems': './src/modules/sub-items/sub.items.module.js',
-        'UniversalDiscovery': './src/modules/universal-discovery/universal.discovery.module.js',
-        'MultiFileUpload': './src/modules/multi-file-upload/multi.file.upload.module.js',
+        SubItems: './src/modules/sub-items/sub.items.module.js',
+        UniversalDiscovery: './src/modules/universal-discovery/universal.discovery.module.js',
+        MultiFileUpload: './src/modules/multi-file-upload/multi.file.upload.module.js',
     },
     output: {
         filename: '[name].module.js',
@@ -15,41 +14,32 @@ module.exports = {
         libraryTarget: 'umd',
         libraryExport: 'default',
     },
-    devtool: 'source-map',
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader'
+                loader: 'babel-loader',
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
     externals: {
-        'react': {
+        react: {
             root: 'React',
             commonjs2: 'react',
             commonjs: 'react',
-            amd: 'react'
+            amd: 'react',
         },
         'react-dom': {
             root: 'ReactDOM',
             commonjs2: 'react-dom',
             commonjs: 'react-dom',
-            amd: 'react-dom'
-        }
+            amd: 'react-dom',
+        },
     },
-    plugins: [
-        new CleanWebpackPlugin(['Resources']),
-        new UglifyJSPlugin({
-            sourceMap: true,
-            uglifyOptions: {
-                ecma: 6
-            }
-        })
-    ]
+    plugins: [new CleanWebpackPlugin(['Resources'])],
 };
