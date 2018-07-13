45046f2ed0106782dcd3bc1120ef0823bf4223df
Piotr Nalepa <piotr.nalepa@ez.no>
2018-07-13 13:57:47 +0200

    Converted stateful popup component into stateless component

--- src/modules/common/popup/popup.component.js
+++ src/modules/common/popup/popup.component.js
@@ -1,54 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './css/popup.component.css';
 
-export default class PopupComponent extends Component {
-    constructor(props) {
-        super(props);
+const PopupComponent = (props) => {
+    const attrs = {
+        className: 'c-popup',
+        hidden: !props.visible,
+    };
 
-        this.state = {
-            visible: props.visible,
-        };
-    }
-
-    UNSAFE_componentWillReceiveProps(props) {
-        this.setState((state) => Object.assign({}, state, { visible: props.visible }));
-    }
-
-    /**
-     * Hides a popup
-     *
-     * @method hidePopup
-     * @memberof PopupComponent
-     */
-    hidePopup() {
-        this.setState((state) => Object.assign({}, state, { visible: false }));
-
-        this.props.onClose();
-    }
-
-    render() {
-        const attrs = {
-            className: 'c-popup',
-            hidden: !this.state.visible,
-        };
-
-        return (
-            <div {...attrs}>
-                <div className="c-popup__header">
-                    <div className="c-popup__title">{this.props.title}</div>
-                    <div className="c-popup__close" onClick={this.hidePopup.bind(this)}>
-                        <svg className="ez-icon">
-                            <use xlinkHref="/bundles/ezplatformadminui/img/ez-icons.svg#discard" />
-                        </svg>
-                    </div>
+    return (
+        <div {...attrs}>
+            <div className="c-popup__header">
+                <div className="c-popup__title">{props.title}</div>
+                <div className="c-popup__close" onClick={props.onClose}>
+                    <svg className="ez-icon">
+                        <use xlinkHref="/bundles/ezplatformadminui/img/ez-icons.svg#discard" />
+                    </svg>
                 </div>
-                <div className="c-popup__content">{this.props.children}</div>
             </div>
-        );
-    }
-}
+            <div className="c-popup__content">{props.children}</div>
+        </div>
+    );
+};
 
 PopupComponent.propTypes = {
     title: PropTypes.string.isRequired,
@@ -60,3 +34,5 @@ PopupComponent.propTypes = {
 PopupComponent.defaultProps = {
     onClose: () => {},
 };
+
+export default PopupComponent;
