5c5933116b3085154c42a0812ffb9f582f1ac777
Piotr Nalepa <piotr.nalepa@ez.no>
2019-04-17 13:01:48 +0200

    Configured environment for testing React components

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    verbose: false,
+    testMatch: ['**/src/modules/udw/__tests__/**/*.[jt]s?(x)', '**/src/modules/udw/?(*.)+(spec|test).[jt]s?(x)'],
+    setupFiles: ['./jest.setup.js'],
+};
