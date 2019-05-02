b69b3a730c34459631f2eb64d90e77a790fcb166
Piotr Nalepa <piotr.nalepa@ez.no>
2019-05-02 14:26:56 +0200

    Refactored the selected content item component

--- src/modules/udw/common/selected-content/selected.content.component.js
+++ src/modules/udw/common/selected-content/selected.content.component.js
@@ -4,48 +4,63 @@ import SelectedContentItemComponent from './selected.content.item.component';
 import PopupComponent from '../../../common/tooltip-popup/tooltip.popup.component';
 import { classnames } from '../../../common/classnames/classnames';
 
+const noConfirmedContentTitle = Translator.trans(
+    /*@Desc("No confirmed content yet")*/ 'select_content.no_confirmed_content.title',
+    {},
+    'universal_discovery_widget'
+);
+const getTitle = (total) => {
+    let title = Translator.trans(/*@Desc("Confirmed items")*/ 'select_content.confirmed_items.title', {}, 'universal_discovery_widget');
+
+    if (total) {
+        title = `${title} (${total})`;
+    }
+
+    return title;
+};
+const renderLimitLabel = (itemsLimit) => {
+    let limitLabel = '';
+
+    if (!!itemsLimit) {
+        const limitLabelText = Translator.trans(
+            /*@Desc("Limit %items% max")*/ 'select_content.limit.label',
+            {
+                items: itemsLimit,
+            },
+            'universal_discovery_widget'
+        );
+
+        limitLabel = <small className="c-selected-content__label--limit">{limitLabelText}</small>;
+    }
+
+    return limitLabel;
+};
+
 const SelectedContentComponent = ({ items, itemsLimit, onItemRemove }) => {
     const [isPopupVisible, setIsPopupVisible] = useState(false);
-    const togglePopup = () => setIsPopupVisible(!isPopupVisible && !!items.length);
+    const itemsCount = items.length;
+    const togglePopup = () => setIsPopupVisible(!isPopupVisible && !!itemsCount);
     const hidePopup = () => setIsPopupVisible(false);
-    const getTitle = () => {
-        let title = Translator.trans(/*@Desc("Confirmed items")*/ 'select_content.confirmed_items.title', {}, 'universal_discovery_widget');
-        const total = items.length;
-
-        if (total) {
-            title = `${title} (${total})`;
-        }
-
-        return title;
-    };
     const renderSelectedItem = (item) => {
-        return <SelectedContentItemComponent key={item.remoteId} data={item} onRemove={onItemRemove} />;
-    };
-    const renderLimitLabel = () => {
-        let limitLabel = '';
-
-        if (!!itemsLimit) {
-            const limitLabelText = Translator.trans(
-                /*@Desc("Limit %items% max")*/ 'select_content.limit.label',
-                {
-                    items: itemsLimit,
-                },
-                'universal_discovery_widget'
-            );
-
-            limitLabel = <small className="c-selected-content__label--limit">{limitLabelText}</small>;
-        }
-
-        return limitLabel;
+        const contentTypeInfo = item.ContentInfo.Content.ContentTypeInfo;
+        const attrs = {
+            key: item.remoteId,
+            contentName: item.ContentInfo.Content.Name,
+            locationId: item.id,
+            contentTypeIdentifie: contentTypeInfo && contentTypeInfo.identifier,
+            contentTypeName: contentTypeInfo && contentTypeInfo.names.value[0]['#text'],
+            onRemove: onItemRemove,
+        };
+        return <SelectedContentItemComponent {...attrs} />;
     };
     const renderSelectedItems = () => {
-        if (!items.length) {
+        if (!itemsCount) {
             return null;
         }
 
         return (
             <div className="c-selected-content-popup">
-                <PopupComponent title={getTitle()} visible={isPopupVisible} onClose={hidePopup}>
+                <PopupComponent title={getTitle(itemsCount)} visible={isPopupVisible} onClose={hidePopup}>
                     {items.map(renderSelectedItem)}
                 </PopupComponent>
             </div>
@@ -54,20 +69,15 @@ const SelectedContentComponent = ({ items, itemsLimit, onItemRemove }) => {
     const titles = items.map((item) => item.ContentInfo.Content.Name).join(', ');
     const btnClassNames = classnames({
         'c-selected-content__info': true,
-        'c-selected-content__info--any-item-selected': !!items.length,
+        'c-selected-content__info--any-item-selected': !!itemsCount,
     });
-    const noConfirmedContentTitle = Translator.trans(
-        /*@Desc("No confirmed content yet")*/ 'select_content.no_confirmed_content.title',
-        {},
-        'universal_discovery_widget'
-    );
 
     return (
         <div className="c-selected-content">
             {renderSelectedItems()}
             <button type="button" className={btnClassNames} onClick={togglePopup}>
-                <strong className="c-selected-content__title">{getTitle()}</strong>
-                {renderLimitLabel()}
+                <strong className="c-selected-content__title">{getTitle(itemsCount)}</strong>
+                {renderLimitLabel(itemsLimit)}
                 <span className="c-selected-content__content-names">{titles.length ? titles : noConfirmedContentTitle}</span>
             </button>
         </div>
--- src/modules/udw/common/selected-content/selected.content.item.component.js
+++ src/modules/udw/common/selected-content/selected.content.item.component.js
@@ -2,34 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Icon from '../../../common/icon/icon';
 
-const SelectedContentItemComponent = ({ data, onRemove }) => {
-    const contentTypeInfo = data.ContentInfo.Content.ContentTypeInfo;
-    const notAvailableLabel = Translator.trans(/*@Desc("N/A")*/ 'select_content.not_available.label', {}, 'universal_discovery_widget');
-    const contentTypeName = contentTypeInfo ? contentTypeInfo.names.value[0]['#text'] : notAvailableLabel;
+const notAvailableLabel = Translator.trans(/*@Desc("N/A")*/ 'select_content.not_available.label', {}, 'universal_discovery_widget');
+const SelectedContentItemComponent = ({ contentName, locationId, contentTypeIdentifier, contentTypeName, onRemove }) => {
     let icon = null;
 
-    if (contentTypeInfo) {
-        icon = <Icon name={contentTypeInfo.identifier} extraClasses="c-selected-content-item__icon ez-icon--small" />;
+    if (contentTypeIdentifier) {
+        icon = <Icon name={contentTypeIdentifier} extraClasses="c-selected-content-item__icon ez-icon--small" />;
     }
 
     return (
         <div className="c-selected-content-item">
             <div className="c-selected-content-item__wrapper">
-                <div className="c-selected-content-item__name">{data.ContentInfo.Content.Name}</div>
+                <div className="c-selected-content-item__name">{contentName}</div>
                 <div className="c-selected-content-item__type">
                     {icon} {contentTypeName}
                 </div>
             </div>
-            <div className="c-selected-content-item__remove" onClick={() => onRemove(data.id)} tabIndex="-1">
+            <button type="button" className="c-selected-content-item__remove" onClick={() => onRemove(locationId)}>
                 <Icon name="discard" extraClasses="ez-icon--light ez-icon--small" />
-            </div>
+            </button>
         </div>
     );
 };
 
 SelectedContentItemComponent.propTypes = {
-    data: PropTypes.object.isRequired,
+    contentName: PropTypes.string.isRequired,
+    locationId: PropTypes.string.isRequired,
     onRemove: PropTypes.func.isRequired,
+    contentTypeName: PropTypes.string,
+    contentTypeIdentifier: PropTypes.string,
+};
+
+SelectedContentItemComponent.defaultProps = {
+    contentTypeIdentifier: null,
+    contentTypeName: notAvailableLabel,
 };
 
 export default SelectedContentItemComponent;
