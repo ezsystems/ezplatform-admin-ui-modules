44b6a994a073d68543b68d36c0698adbf6d7c3db
Jakub Brzegowski <symfiz@gmail.com>
2019-05-30 11:35:54 +0200

    Revert "EZP-30530: Create a base for the new UDW (#186)"
    This reverts commit 2baf3d02d27d8f093daf849d7da8519fcf68769f.

--- Resources/encore/ez.config.js
+++ Resources/encore/ez.config.js
@@ -1,15 +1,14 @@
 const path = require('path');
 
 module.exports = (Encore) => {
-    Encore.addEntry('ezplatform-admin-ui-modules-udw-js', [
-        path.resolve(__dirname, '../../src/modules/universal-discovery/universal.discovery.module.js'),
-    ])
-        .addEntry('ezplatform-admin-ui-modules-mfu-js', [
-            path.resolve(__dirname, '../../src/modules/multi-file-upload/multi.file.upload.module.js'),
-        ])
-        .addEntry('ezplatform-admin-ui-modules-subitems-js', [path.resolve(__dirname, '../../src/modules/sub-items/sub.items.module.js')])
-        .addEntry('ezplatform-admin-ui-modules-content-tree-js', [
-            path.resolve(__dirname, '../../src/modules/content-tree/content.tree.module.js'),
-        ])
-        .addEntry('ezplatform-admin-ui-modules-udw-v2-js', [path.resolve(__dirname, '../../src/modules/udw/udw.module.js')]);
+	Encore.addEntry('ezplatform-admin-ui-modules-udw-js', [
+		path.resolve(__dirname, '../../src/modules/universal-discovery/universal.discovery.module.js'),
+	])
+		.addEntry('ezplatform-admin-ui-modules-mfu-js', [
+			path.resolve(__dirname, '../../src/modules/multi-file-upload/multi.file.upload.module.js'),
+		])
+		.addEntry('ezplatform-admin-ui-modules-subitems-js', [path.resolve(__dirname, '../../src/modules/sub-items/sub.items.module.js')])
+		.addEntry('ezplatform-admin-ui-modules-content-tree-js', [
+			path.resolve(__dirname, '../../src/modules/content-tree/content.tree.module.js'),
+		]);
 };
