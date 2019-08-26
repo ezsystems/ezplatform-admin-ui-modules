2e5463e3521d701c3584d1a786c027e1b5efdf90
Dariusz Szut <dew326@gmail.com>
2019-08-26 13:33:27 +0200

    EZP-30825: Convert Bookmarks Tab as a standalone ReactJS module (#205)

--- src/modules/ud/bookmarks.tab.module.js
+++ src/modules/ud/bookmarks.tab.module.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 
 import Tab from './components/tab/tab';
+import BookmarksList from './components/bookmarks-list/bookmarks.list';
+import GridView from './components/grid-view/grid.view';
+import Finder from './components/finder/finder';
+import ContentTree from './components/content-tree/content.tree';
+
+import { CurrentViewContext, MarkedLocationContext } from './universal.discovery.module';
+
+const views = {
+    grid: <GridView />,
+    finder: <Finder />,
+    'content-tree': <ContentTree />,
+};
 
 const BookmarksTabModule = () => {
+    const [currentView, setCurrentView] = useContext(CurrentViewContext);
+    const [markedLocation, setMarkedLocation] = useContext(MarkedLocationContext);
+    const isFirstRun = useRef(true);
+    const renderBrowseLocations = () => {
+        if (!markedLocation) {
+            return null;
+        }
+
+        return views[currentView];
+    };
+
+    useEffect(() => {
+        if (isFirstRun.current) {
+            isFirstRun.current = false;
+
+            setMarkedLocation(null);
+        }
+    });
+
     return (
         <div className="m-bookmarks-tab">
-            <Tab>Bookmarks</Tab>
+            <Tab>
+                <BookmarksList />
+                {renderBrowseLocations()}
+            </Tab>
         </div>
     );
 };
