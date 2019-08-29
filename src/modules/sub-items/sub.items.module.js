8ca53b3c066eace97fc9bed2710be0a14ecb028a
Dawid Parafiński <dawid.parafinski@ez.no>
2019-08-29 08:48:47 +0200

    access helpers from window global

--- src/modules/sub-items/sub.items.module.js
+++ src/modules/sub-items/sub.items.module.js
@@ -697,7 +697,7 @@ export default class SubItemsModule extends Component {
         const failedItemsData = failedLocations.map(({ id: locationId }) => {
             const item = selectedItems.get(locationId);
             const contentType = contentTypesMap[item.content.ContentType._href];
-            const contentTypeName = eZ.helpers.contentType.getContentTypeName(contentType.identifier);
+            const contentTypeName = window.eZ.helpers.contentType.getContentTypeName(contentType.identifier);
 
             return {
                 contentTypeName,
