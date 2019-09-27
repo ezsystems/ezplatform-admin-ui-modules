e75d9c88d599ccfbaf104fb6683fd0008b5158e6
Jakub <symfiz@gmail.com>
2019-09-27 11:19:04 +0200

    EZP-30972: Make Subitems header checkbox three-state (#215)

--- src/modules/sub-items/components/table-view/table.view.component.js
+++ src/modules/sub-items/components/table-view/table.view.component.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import TableViewItemComponent from './table.view.item.component';
 import TableViewColumnsTogglerComponent from './table.view.columns.toggler';
+import ThreeStateCheckboxComponent from '../three-state-checkbox/three.state.checkbox.component';
 
 const KEY_CONTENT_NAME = 'ContentName';
 const KEY_DATE_MODIFIED = 'DateModified';
@@ -90,12 +91,11 @@ export default class TableViewComponent extends Component {
 
     /**
      * Selects all visible items
-     *
-     * @param {Event} event
      */
-    selectAll(event) {
-        const { toggleAllItemsSelect } = this.props;
-        const isSelectAction = event.target.checked;
+    selectAll() {
+        const { toggleAllItemsSelect, selectedLocationsIds } = this.props;
+        const anyLocationSelected = !!selectedLocationsIds.size;
+        const isSelectAction = !anyLocationSelected;
 
         toggleAllItemsSelect(isSelectAction);
     }
@@ -231,12 +231,18 @@ export default class TableViewComponent extends Component {
         const { columnsVisibility } = this.state;
         const { selectedLocationsIds } = this.props;
         const anyLocationSelected = !!selectedLocationsIds.size;
+        const allLocationsSelected = selectedLocationsIds.size === items.length;
+        const isCheckboxIndeterminate = anyLocationSelected && !allLocationsSelected;
 
         return (
             <thead className={headClass}>
                 <tr className="c-table-view__row">
                     <th className={`${TABLE_HEAD_CLASS} ${cellClass}--checkbox`}>
-                        <input type="checkbox" checked={anyLocationSelected} onChange={this.selectAll} />
+                        <ThreeStateCheckboxComponent
+                            indeterminate={isCheckboxIndeterminate}
+                            checked={anyLocationSelected}
+                            onClick={this.selectAll} // We need onClick, because MS Edge does not trigger onChange when checkbox has indeterminate state. (ref: https://stackoverflow.com/a/33529024/5766602)
+                        />
                     </th>
                     <th className={TABLE_HEAD_CLASS} />
                     <th
