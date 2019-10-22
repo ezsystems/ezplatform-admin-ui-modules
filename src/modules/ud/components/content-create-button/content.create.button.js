4bbe9c832f4afd10abc496db289f1a689fd30257
Dariusz Szut <dew326@gmail.com>
2019-10-22 14:32:44 +0200

    EZP-30980: As an Editor, I want Create Tab to be permission aware

--- src/modules/ud/components/content-create-button/content.create.button.js
+++ src/modules/ud/components/content-create-button/content.create.button.js
@@ -3,17 +3,24 @@ import PropTypes from 'prop-types';
 
 import Icon from '../../../common/icon/icon';
 
-import { CreateContentWidgetContext } from '../../universal.discovery.module';
+import { CreateContentWidgetContext, MarkedLocationContext, LoadedLocationsMapContext } from '../../universal.discovery.module';
 
 const ContentCreateButton = ({ isDisabled }) => {
+    const [markedLocation, setMarkedLocation] = useContext(MarkedLocationContext);
+    const [loadedLocationsMap, dispatchLoadedLocationsAction] = useContext(LoadedLocationsMapContext);
     const [createContentVisible, setCreateContentVisible] = useContext(CreateContentWidgetContext);
     const toggleContentCreateVisibility = () => {
         setCreateContentVisible((prevState) => !prevState);
     };
+    const selectedLocation = loadedLocationsMap.find((loadedLocation) => loadedLocation.parentLocationId === markedLocation);
+    const hasAccess = !selectedLocation.permissions || (selectedLocation.permissions && selectedLocation.permissions.hasAccess);
 
     return (
         <div className="c-content-create-button">
-            <button className="c-content-create-button__btn btn btn-primary" disabled={isDisabled} onClick={toggleContentCreateVisibility}>
+            <button
+                className="c-content-create-button__btn btn btn-primary"
+                disabled={isDisabled || !hasAccess}
+                onClick={toggleContentCreateVisibility}>
                 <Icon name="create" extraClasses="ez-icon--medium ez-icon--light" />
             </button>
         </div>
