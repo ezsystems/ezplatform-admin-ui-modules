0c4a265dff68a7c7ac590cc8e761f7297ad3d260
Dariusz Szut <dew326@gmail.com>
2019-11-15 09:05:39 +0100

    EZP-31065: As a Developer, I want UDW to be configurable (#226)

--- src/modules/ud/components/finder/finder.leaf.js
+++ src/modules/ud/components/finder/finder.leaf.js
@@ -5,12 +5,28 @@ import ToggleSelectionButton from '../toggle-selection-button/toggle.selection.b
 import Icon from '../../../common/icon/icon';
 
 import { createCssClassNames } from '../../../common/helpers/css.class.names';
-import { MarkedLocationContext, LoadedLocationsMapContext, ContentTypesMapContext } from '../../universal.discovery.module';
+import {
+    MarkedLocationContext,
+    LoadedLocationsMapContext,
+    ContentTypesMapContext,
+    SelectedLocationsContext,
+    MultipleConfigContext,
+    ContainersOnlyContext,
+    AllowedContentTypesContext,
+} from '../../universal.discovery.module';
 
 const FinderLeaf = ({ location }) => {
     const [markedLocation, setMarkedLocation] = useContext(MarkedLocationContext);
     const [loadedLocationsMap, dispatchLoadedLocationsAction] = useContext(LoadedLocationsMapContext);
     const contentTypesMap = useContext(ContentTypesMapContext);
+    const [selectedLocations, dispatchSelectedLocationsAction] = useContext(SelectedLocationsContext);
+    const [multiple, multipleItemsLimit] = useContext(MultipleConfigContext);
+    const containersOnly = useContext(ContainersOnlyContext);
+    const allowedContentTypes = useContext(AllowedContentTypesContext);
+    const contentTypeInfo = contentTypesMap[location.ContentInfo.Content.ContentType._href];
+    const isContainer = contentTypeInfo.isContainer;
+    const isNotSelectable =
+        (containersOnly && !isContainer) || (allowedContentTypes && !allowedContentTypes.includes(contentTypeInfo.identifier));
     const markLocation = ({ nativeEvent }) => {
         const isSelectionButtonClicked = nativeEvent.target.closest('.c-toggle-selection-button');
 
@@ -21,11 +37,24 @@ const FinderLeaf = ({ location }) => {
         setMarkedLocation(location.id);
         dispatchLoadedLocationsAction({ type: 'CUT_LOCATIONS', locationId: markedLocation });
         dispatchLoadedLocationsAction({ type: 'UPDATE_LOCATIONS', data: { parentLocationId: location.id, subitems: [] } });
+
+        if (!multiple && !isNotSelectable) {
+            dispatchSelectedLocationsAction({ type: 'CLEAR_SELECTED_LOCATIONS' });
+            dispatchSelectedLocationsAction({ type: 'ADD_SELECTED_LOCATION', location });
+        }
+    };
+    const renderToggleSelectionButton = () => {
+        if (!multiple || isNotSelectable) {
+            return null;
+        }
+
+        return <ToggleSelectionButton location={location} />;
     };
     const className = createCssClassNames({
         'c-finder-leaf': true,
         'c-finder-leaf--marked': !!loadedLocationsMap.find((loadedLocation) => loadedLocation.parentLocationId === location.id),
         'c-finder-leaf--has-children': !!location.childCount,
+        'c-finder-leaf--not-selectable': isNotSelectable,
     });
 
     return (
@@ -37,7 +66,7 @@ const FinderLeaf = ({ location }) => {
                 />
                 {location.ContentInfo.Content.Name}
             </span>
-            <ToggleSelectionButton location={location} />
+            {renderToggleSelectionButton()}
         </div>
     );
 };
