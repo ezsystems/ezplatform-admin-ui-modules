701d1087c17a45ef73f63ac1327ec8af48aebbf2
Lukasz Ostafin <lukasz.ostafin@ez.no>
2020-02-12 09:32:56 +0100

    Fixed merge up

--- src/modules/universal-discovery/universal.discovery.module.js
+++ src/modules/universal-discovery/universal.discovery.module.js
@@ -107,9 +107,8 @@ const UniversalDiscoveryModule = (props) => {
 
         loadContentTypes(restInfo, handleLoadContentTypes);
         window.document.body.dispatchEvent(new CustomEvent('ez-udw-opened'));
-        console.log('open');
+
         return () => {
-            console.log('closed');
             window.document.body.dispatchEvent(new CustomEvent('ez-udw-closed'));
         };
     }, []);
