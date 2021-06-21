a9149aee84ef4eaa608ed4d6d2f288af6c58ef7f
Adam Wójs <adam@wojs.pl>
2021-06-21 07:51:18 +0200

    Applied code review suggestions

--- src/modules/multi-file-upload/services/multi.file.upload.service.js
+++ src/modules/multi-file-upload/services/multi.file.upload.service.js
@@ -145,21 +145,15 @@ const prepareStruct = ({ parentInfo, config, languageCode }, data) => {
             const fileValue = {
                 fileName: data.file.name,
                 data: data.fileReader.result.replace(/^.*;base64,/, ''),
-            }
+            };
 
             if (data.file.type.startsWith('image/')) {
                 fileValue.alternativeText = data.file.name;
             }
 
             const fields = [
-                {
-                    fieldDefinitionIdentifier: mapping.nameFieldIdentifier,
-                    fieldValue: data.file.name
-                },
-                {
-                    fieldDefinitionIdentifier: mapping.contentFieldIdentifier,
-                    fieldValue: fileValue
-                },
+                { fieldDefinitionIdentifier: mapping.nameFieldIdentifier, fieldValue: data.file.name },
+                { fieldDefinitionIdentifier: mapping.contentFieldIdentifier, fieldValue: fileValue },
             ];
 
             const struct = {
